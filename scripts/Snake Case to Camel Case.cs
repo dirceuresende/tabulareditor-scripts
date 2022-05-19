34799c72cd69000d1b2994b5dd463c5900cc5c0e
Dirceu Resende <dirceu.resende@yahoo.com.br>
2022-05-19 11:11:30 -0400

    Create Snake Case to Camel Case.cs

---
+++ scripts/Snake Case to Camel Case.cs
@@ -0,0 +1,53 @@
+foreach(var obj in Model.Tables.SelectMany(t => t.Columns)) {
+    
+    var oldName = obj.Name;
+    var newName = new System.Text.StringBuilder();
+    
+    for(int i = 0; i < oldName.Length; i++) {
+        
+        // First letter should always be capitalized:
+        if(i == 0) {
+            newName.Append(Char.ToUpper(oldName[i]));
+        }
+        else if(oldName[i - 1] == '_')
+        {
+            newName.Append(char.ToUpper(oldName[i]));
+        }
+        else
+        {
+            newName.Append(char.ToLower(oldName[i]));
+        }
+    }
+    
+    obj.Name = newName.Replace("cao", "ção").Replace("_", " ").ToString();
+    
+}
+
+
+
+foreach (var obj in Model.Tables) 
+{
+    
+    var oldName = obj.Name;
+    var newName = new System.Text.StringBuilder();
+    
+    for(int i = 0; i < oldName.Length; i++) {
+        
+        // First letter should always be capitalized:
+        if(i == 0) {
+            newName.Append(Char.ToUpper(oldName[i]));
+        }
+        else if(oldName[i - 1] == '_')
+        {
+            newName.Append(char.ToUpper(oldName[i]));
+            newName.Append(" ");
+        }
+        else
+        {
+            newName.Append(char.ToLower(oldName[i]));
+        }
+    }
+    
+    obj.Name = newName.Replace("cao", "ção").Replace("_", " ").ToString();
+    
+}
