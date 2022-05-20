90b6d183d0e64b0a55cf262774c23ee25a5ecd90
Dirceu Resende <dirceu.resende@yahoo.com.br>
2022-05-20 09:13:12 -0400

    Update Snake Case to Camel Case.cs

--- scripts/Snake Case to Camel Case.cs
+++ scripts/Snake Case to Camel Case.cs
@@ -9,7 +9,7 @@ foreach(var obj in Model.Tables.SelectMany(t => t.Columns)) {
         if(i == 0) {
             newName.Append(Char.ToUpper(oldName[i]));
         }
-        else if(oldName[i - 1] == '_')
+        else if(oldName[i - 1] == '_' || oldName[i - 1] == ' ')
         {
             newName.Append(char.ToUpper(oldName[i]));
         }
@@ -37,7 +37,34 @@ foreach (var obj in Model.Tables)
         if(i == 0) {
             newName.Append(Char.ToUpper(oldName[i]));
         }
-        else if(oldName[i - 1] == '_')
+        else if(oldName[i - 1] == '_' || oldName[i - 1] == ' ')
+        {
+            newName.Append(char.ToUpper(oldName[i]));
+        }
+        else
+        {
+            newName.Append(char.ToLower(oldName[i]));
+        }
+    }
+    
+    obj.Name = newName.Replace("cao", "ção").Replace("_", " ").ToString();
+    
+}
+
+
+foreach (var obj in Model.AllMeasures) 
+{
+    
+    var oldName = obj.Name;
+    var newName = new System.Text.StringBuilder();
+    
+    for(int i = 0; i < oldName.Length; i++) {
+        
+        // First letter should always be capitalized:
+        if(i == 0) {
+            newName.Append(Char.ToUpper(oldName[i]));
+        }
+        else if(oldName[i - 1] == '_' || oldName[i - 1] == ' ')
         {
             newName.Append(char.ToUpper(oldName[i]));
         }
